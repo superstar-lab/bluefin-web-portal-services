641fe715545c186357d080ed1d9cf78ab425d300
topdeveloper1228 <topdeveloper1228@gmail.com>
2016-07-26 11:40:35 -0600

    #7038 - BE - Create API to register api consumers (third party application clients)
    - Bug fixing
    
    Signed-off-by: Natalia Quiros <nquiros@internal.mcmcg.com>

--- src/main/java/com/mcmcg/ico/bluefin/service/util/querydsl/Predicate.java
+++ src/main/java/com/mcmcg/ico/bluefin/service/util/querydsl/Predicate.java
@@ -7,6 +7,7 @@ import java.math.BigDecimal;
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.Date;
