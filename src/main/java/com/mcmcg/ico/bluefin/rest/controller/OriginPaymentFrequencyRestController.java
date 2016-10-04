0e304f0e7d87729ac8cf90cc6a888fe213aa02cf
topdeveloper1228 <topdeveloper1228@gmail.com>
2016-10-04 07:39:49 -0600

    #7814 - BE - Application and OriginPaymentFrequency services
    - Application lookup service to retrieve all information on this table
    - OriginPaymentFrequency lookup service to retrieve all information on this table
    - Swagger UI documentation
    
    Signed-off-by: Natalia Quiros <natalia.quiros@mcmcg.com>

--- src/main/java/com/mcmcg/ico/bluefin/rest/controller/OriginPaymentFrequencyRestController.java
+++ src/main/java/com/mcmcg/ico/bluefin/rest/controller/OriginPaymentFrequencyRestController.java
@@ -5,49 +5,40 @@ import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.mcmcg.ico.bluefin.persistent.Application;
+import com.mcmcg.ico.bluefin.persistent.OriginPaymentFrequency;
 import com.mcmcg.ico.bluefin.persistent.Role;
 import com.mcmcg.ico.bluefin.rest.resource.ErrorResource;
-import com.mcmcg.ico.bluefin.service.ApplicationService;
+import com.mcmcg.ico.bluefin.service.OriginPaymentFrequencyService;
 
 import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
+import springfox.documentation.annotations.ApiIgnore;
 
 @RestController
-@RequestMapping(value = "/api")
-public class ApplicationRestController {
-    
+@RequestMapping(value = "/api/origin-payment-frequencies")
+public class OriginPaymentFrequencyRestController {
+
     @Autowired
-    private ApplicationService applicationService;
+    private OriginPaymentFrequencyService originPaymentFrequencyService;
+    private static final Logger LOGGER = LoggerFactory.getLogger(OriginPaymentFrequencyRestController.class);
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(ApplicationRestController.class);
-
-    @RequestMapping(method = RequestMethod.GET, value = "/ping", produces = "application/json")
-    public ResponseEntity<String> ping(@RequestParam(value = "param", required = false) String param) {
-        LOGGER.debug("Status of the application endpoint. Param = [{}]", param);
-
-        return new ResponseEntity<String>("{ \"status\" : \"UP\" }", HttpStatus.OK);
-    }
-    
-    @ApiOperation(value = "getApplications", nickname = "Get applications")
-    @RequestMapping(method = RequestMethod.GET, value = "/applications", produces = "application/json")
+    @ApiOperation(value = "getOriginPaymentFrequencies", nickname = "Get Origin Payment Frequencies")
+    @RequestMapping(method = RequestMethod.GET, produces = "application/json")
     @ApiImplicitParam(name = "X-Auth-Token", value = "Authorization token", dataType = "string", paramType = "header")
     @ApiResponses(value = { @ApiResponse(code = 200, message = "OK", response = Role.class, responseContainer = "List"),
             @ApiResponse(code = 400, message = "Bad Request", response = ErrorResource.class),
             @ApiResponse(code = 401, message = "Unauthorized", response = ErrorResource.class),
             @ApiResponse(code = 403, message = "Forbidden", response = ErrorResource.class),
             @ApiResponse(code = 500, message = "Internal Server Error", response = ErrorResource.class) })
-    public List<Application> get() {
-        LOGGER.info("Getting all applications");
-        return applicationService.getApplications();
+    public List<OriginPaymentFrequency> get(@ApiIgnore Authentication authentication) {
+        LOGGER.info("Getting Origin Payment Frequencies");
+        return originPaymentFrequencyService.getOriginPaymentFrequencies();
     }
 }
---
+++ src/main/java/com/mcmcg/ico/bluefin/service/OriginPaymentFrequencyService.java
@@ -0,0 +1,29 @@
+package com.mcmcg.ico.bluefin.service;
+
+import java.util.List;
+
+import javax.transaction.Transactional;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.mcmcg.ico.bluefin.persistent.OriginPaymentFrequency;
+import com.mcmcg.ico.bluefin.persistent.jpa.OriginPaymentFrequencyRepository;
+
+@Service
+@Transactional
+public class OriginPaymentFrequencyService {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(BatchUploadService.class);
+
+    @Autowired
+    private OriginPaymentFrequencyRepository originPaymentFrequencyRepository;
+
+    public List<OriginPaymentFrequency> getOriginPaymentFrequencies() {
+        LOGGER.info("Getting Origin Payment Frequency list.");
+        return originPaymentFrequencyRepository.findAll();
+    }
+
+}
