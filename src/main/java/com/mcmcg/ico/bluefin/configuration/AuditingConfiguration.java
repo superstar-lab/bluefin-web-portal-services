1abcbf1d781f06a546bb14389332d8819159715d
topdeveloper1228 <topdeveloper1228@gmail.com>
2017-04-16 11:22:11 +0530

    #ORM_Refactoring
    POM files JPA dependencies, @EnableJPAAuditing at
    AuditingConfiguration.java

--- src/main/java/com/mcmcg/ico/bluefin/configuration/AuditingConfiguration.java
+++ src/main/java/com/mcmcg/ico/bluefin/configuration/AuditingConfiguration.java
@@ -3,14 +3,14 @@ package com.mcmcg.ico.bluefin.configuration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.domain.AuditorAware;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+//import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 
 import com.mcmcg.ico.bluefin.security.model.SecurityUser;
 
 @Configuration
-@EnableJpaAuditing
+//@EnableJpaAuditing
 public class AuditingConfiguration {
 
     @Bean
