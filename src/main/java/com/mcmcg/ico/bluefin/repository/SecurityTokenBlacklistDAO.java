383055a3eb9e22b7238a53d0740d7c8993a9aaa5
topdeveloper1228 <topdeveloper1228@gmail.com>
2017-04-20 19:27:34 +0530

    #ORM_Refactoring
    Code cleanup - Dheeraj

--- src/main/java/com/mcmcg/ico/bluefin/repository/SecurityTokenBlacklistDAO.java
+++ src/main/java/com/mcmcg/ico/bluefin/repository/SecurityTokenBlacklistDAO.java
@@ -5,7 +5,11 @@ import com.mcmcg.ico.bluefin.model.SecurityTokenBlacklist;
 public interface SecurityTokenBlacklistDAO {
 	SecurityTokenBlacklist findByTokenId(long tokenId);
 
+	SecurityTokenBlacklist findByToken(String token);
+
 	SecurityTokenBlacklist findByUserIdAndToken(long userId, String token);
+
+	SecurityTokenBlacklist findByUserIdAndType(long userId, String type);
 
 	long saveSecurityTokenBlacklist(SecurityTokenBlacklist securityTokenBlacklist);
 }
