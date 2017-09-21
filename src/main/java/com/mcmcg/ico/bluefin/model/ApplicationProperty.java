a532a18c20901fab40dc583bd5fdd52801521196
topdeveloper1228 <topdeveloper1228@gmail.com>
2017-09-21 14:02:57 +0530

    Changed API URL and new insert and update API for application properties

---
+++ src/main/java/com/mcmcg/ico/bluefin/model/ApplicationProperty.java
@@ -0,0 +1,26 @@
+package com.mcmcg.ico.bluefin.model;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import lombok.Data;
+
+@Data
+public class ApplicationProperty extends Common {
+
+	@JsonProperty("applicationPropertyId")
+	private Long propertyId;
+	@JsonProperty("applicationPropertyName")
+	private String propertyName;
+	@JsonProperty("applicationPropertyValue")
+	private String propertyValue;
+	@JsonProperty("dataType")
+	private String applicationDataType;
+	@JsonProperty("description")
+	private String applicationDescription;
+	@JsonProperty("modifiedBy")
+	private String modifiedByUser;
+
+	public ApplicationProperty() {
+		// Default Constructor
+	}
+}
--- src/main/java/com/mcmcg/ico/bluefin/repository/PropertyDAO.java
+++ src/main/java/com/mcmcg/ico/bluefin/repository/PropertyDAO.java
@@ -2,11 +2,14 @@ package com.mcmcg.ico.bluefin.repository;
 
 import java.util.List;
 
+import com.mcmcg.ico.bluefin.model.ApplicationProperty;
 import com.mcmcg.ico.bluefin.model.Property;
 
 public interface PropertyDAO {
 	public Property findByName(String name);
 	public String getPropertyValue(String propertyName);
-	public List<Property> findAll();
-	public List<Property> getAllProperty();
+	public List<ApplicationProperty> findAll();
+	public List<ApplicationProperty> getAllProperty();
+	public ApplicationProperty saveOrUpdate(ApplicationProperty applicationProperty);
+	public ApplicationProperty saveOrUpdateProperty(ApplicationProperty applicationProperty);
 }
