7be9c047915112ec7c6eead04088317600ce7f95
topdeveloper1228 <topdeveloper1228@gmail.com>
2019-06-06 15:08:07 +0530

    #WI175568
    Remove unused import

--- src/main/java/com/mcmcg/ico/bluefin/batch/file/ACFBatchReturnFile.java
+++ src/main/java/com/mcmcg/ico/bluefin/batch/file/ACFBatchReturnFile.java
@@ -28,8 +28,6 @@ import org.joda.time.format.DateTimeFormat;
 import org.joda.time.format.DateTimeFormatter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.FileSystemResource;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -41,15 +39,11 @@ import com.mcmcg.ico.bluefin.factory.BatchReturnFile;
 import com.mcmcg.ico.bluefin.model.BatchFileObjects;
 import com.mcmcg.ico.bluefin.model.SaleTransaction;
 import com.mcmcg.ico.bluefin.rest.controller.exception.CustomException;
-import com.mcmcg.ico.bluefin.service.PropertyService;
 
 @Component
 public class ACFBatchReturnFile extends BatchReturnFile {
 
 	private static final Logger LOGGER = LoggerFactory.getLogger(ACFBatchReturnFile.class);
-	
-	@Autowired
-	private PropertyService propertyService;
 	
 	// Delimiter used in CSV file
 	private static final String NEW_LINE_SEPARATOR = "\n";
