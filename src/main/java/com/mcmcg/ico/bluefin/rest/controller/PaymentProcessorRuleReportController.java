7c058c74506e0cbd0aa9f81ae774b046533649d0
topdeveloper1228 <topdeveloper1228@gmail.com>
2019-10-14 07:53:31 +0000

    Change request mapping url

--- src/main/java/com/mcmcg/ico/bluefin/rest/controller/PaymentProcessorRuleReportController.java
+++ src/main/java/com/mcmcg/ico/bluefin/rest/controller/PaymentProcessorRuleReportController.java
@@ -20,7 +20,7 @@ import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
 
 @RestController
-@RequestMapping(value = "/api/payment-processor-rule-reports")
+@RequestMapping(value = "/api/payment-processor-rule-trends")
 public class PaymentProcessorRuleReportController {
 
 	@Autowired
