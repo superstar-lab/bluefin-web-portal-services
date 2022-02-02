04338759bacc140c46bff3c3433b810ee68e4a9a
topdeveloper1228 <topdeveloper1228@gmail.com>
2022-02-02 11:20:25 +0000

    Merged PR 26809: Bluefin portal improvement
    Related work items: #316406

---
+++ src/test/java/com/mcmcg/ico/bluefin/repository/AccountValidationDAOImplUT.java
@@ -0,0 +1,26 @@
+package com.mcmcg.ico.bluefin.repository;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class AccountValidationDAOImplUT {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void findAll() {
+    }
+
+    @Test
+    public void findAllFilter() {
+    }
+}
